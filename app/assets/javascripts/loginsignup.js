9ca0ca6dceff9a6e167bd21832cf8b1169ee0b46
Sanket <singhsanket143@gmail.com>
2017-09-13 18:13:00 +0530

    Login updated

--- app/assets/javascripts/loginsignup.js
+++ app/assets/javascripts/loginsignup.js
@@ -1,24 +1,46 @@
-$(document).ready(
-    function() {
+$(document).ready(function(){
+    $('.signup-slider').slick({
+        dots: true,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 2000
+    });
 
-        $('#signup').on("click", function() {
-            var x = this.id;
-            $("#" + x).removeClass("s-atbottom");
-            $("#" + x).addClass("s-attop");
-            $("#login").removeClass("l-attop");
-            $("#login").addClass("l-atbottom");
+    $("img").height($(".main-box").height());
 
-        });
+    $(".to-signin").on("click", function () {
+        $(this)
+            .addClass("top-active-button")
+            .siblings()
+            .removeClass("top-active-button");
+        $(".form-signup").slideUp(500);
+        $(".form-signin").slideDown(500);
+    });
 
-        $('#login').on("click", function() {
-            var x = this.id;
-            $("#" + x).removeClass("l-atbottom");
-            $("#" + x).addClass("l-attop");
-            $("#signup").removeClass("s-attop");
-            $("#signup").addClass("s-atbottom");
+    $(".to-signup").on("click", function () {
+        $(this)
+            .addClass("top-active-button")
+            .siblings()
+            .removeClass("top-active-button");
+        $(".form-signin").slideUp(500);
+        $(".form-signup").slideDown(500);
+    });
 
-        });
+    $(".to-signin-link").on("click", function () {
+        $(".to-signin")
+            .addClass("top-active-button")
+            .siblings()
+            .removeClass("top-active-button");
+        $(".form-signup").slideUp(200);
+        $(".form-signin").slideDown(200);
+    });
 
-    }
-
-);
+    $(".to-signup-link").on("click", function () {
+        $(".to-signup")
+            .addClass("top-active-button")
+            .siblings()
+            .removeClass("top-active-button");
+        $(".form-signin").slideUp(200);
+        $(".form-signup").slideDown(200);
+    });
+});
